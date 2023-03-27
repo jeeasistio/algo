0acc8666fc59de332156c8f1fbc84a4655f81bd5
jeeasistio <jeeasistio08@gmail.com>
2023-03-28 01:09:03 +0800

    updated count_unreachable_nodes

--- src/count_unreachable_nodes.ts
+++ src/count_unreachable_nodes.ts
@@ -1,39 +1,56 @@
 export const countUnreachableNodes = (num: number, edges: [number, number][]): number => {
-    let newIndex = 0
-    const mapping: Record<number, number> = {}
-    edges.forEach((edge) => {
-        if (mapping[edge[0]] > -1) {
-            if (mapping[edge[1]] > -1) return
-            mapping[edge[1]] = mapping[edge[0]]
-        } else {
-            if (mapping[edge[1]] > -1) {
-                mapping[edge[0]] = mapping[edge[1]]
-                return
-            }
-            mapping[edge[0]] = newIndex
-            mapping[edge[1]] = newIndex
-            newIndex += 1
-        }
-    })
-    let sum = 0
-    const connections = Object.entries(mapping).reduce((acc, [key, val]) => {
-        if (!acc[Number(val)]) {
-            acc[Number(val)] = 0
-        }
-        acc[Number(val)] += 1
-        sum += 1
+    const neighbors = createNeighbors(edges)
+    const connections = createConnectedNodes(neighbors)
+    const filledConnections = fillConnections(connections, num)
+    return filledConnections.reduce((acc, val) => {
+        acc += (val * (num - val)) / 2
         return acc
-    }, [] as number[])
+    }, 0)
+}
+
+const createNeighbors = (edges: [number, number][]) => {
+    return edges.reduce((acc, edge) => {
+        if (!acc[edge[0]]) acc[edge[0]] = []
+        acc[edge[0]].push(edge[1])
+        if (!acc[edge[1]]) acc[edge[1]] = []
+        acc[edge[1]].push(edge[0])
+        return acc
+    }, {} as Record<string, number[]>)
+}
+
+const createConnectedNodes = (neighbors: ReturnType<typeof createNeighbors>) => {
+    const visited: Record<string, boolean> = {}
+    const connections: number[] = []
+    const stack: number[] = []
+    let count = 0
+    Object.entries(neighbors).forEach(([val, nghbrs]) => {
+        if (!visited[val]) {
+            visited[val] = true
+            stack.push(...nghbrs)
+            count += 1
+        }
+        while (stack.length > 0) {
+            const next = (stack.pop() as number)?.toString()
+            if (!visited[next]) {
+                visited[next] = true
+                stack.push(...neighbors[next])
+                count += 1
+            }
+        }
+        if (count > 0) connections.push(count)
+        count = 0
+    })
+    return connections
+}
+
+const fillConnections = (connections: ReturnType<typeof createConnectedNodes>, num: number) => {
+    const connectionsCopy = [...connections]
+    let sum = connections.reduce((acc, val) => (acc += val), 0)
     while (sum !== num) {
-        connections.push(1)
+        connectionsCopy.push(1)
         sum += 1
     }
-    return (
-        connections.reduce((acc, val) => {
-            acc += val * (num - val)
-            return acc
-        }, 0) / 2
-    )
+    return connectionsCopy
 }
 
 console.log(
@@ -43,5 +60,5 @@ console.log(
         [2, 4],
         [1, 6],
         [5, 4],
-    ])
+    ] as [number, number][])
 )
